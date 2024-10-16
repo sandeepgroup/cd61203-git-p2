0629ab13f514d153bbc8f87b91392c81ca3558e6
Sandeep Reddy <kumar0020@gmail.com>
2024-10-16 20:04:27 +0530

    added division

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+def divide(x, y):
+    if y == 0:
+        return "Error! Division by zero."
+    return x / y
+
 if __name__ == "__main__":
     a = 10
     b = 5
+
+    print("Division:", divide(a, b))
 
